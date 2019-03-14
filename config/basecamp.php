306da01799570ebe160c73b0eb18e8cfeecee1da
sloanja <jacob@thesloanzone.com>
2019-03-13 20:19:47 -0500

    Update basecamp.php

--- config/basecamp.php
+++ config/basecamp.php
@@ -4,12 +4,12 @@ return [
 
    /*
     |--------------------------------------------------------------------------
-    | Basecamp 3 App User-Agent
+    | Basecamp 2 App User-Agent
     |--------------------------------------------------------------------------
     |
     | You must enter a User-Agent header, with both the name of your app and
     | a link to your website or an email. For more details, you can visit
-    | https://github.com/basecamp/bc3-api#identifying-your-application
+    | https://github.com/basecamp/bcx-api#identify-your-app
     */
 
     'user-agent' => '',
